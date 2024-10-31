f3a1f040adc6d392c730298c93e379ef5222b9a1
Usman-17 <usmandev580@gmail.com>
2024-10-31 23:39:50 +0500

    feat: Implement fetch user functionality and display navigation links when user is logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,42 @@
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import LoginPage from "./pages/LoginPage";
 import Homepage from "./pages/Homepage";
 import LicenseListPage from "./pages/LicenseListPage";
+import { useQuery } from "@tanstack/react-query";
 
 export default function App() {
+  const { data: authUser } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (data.error) return null;
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
           <Route path="/licenses/manage" element={<LicenseListPage />} />
         </Routes>
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[8vw]">
-      <div className="flex items-center justify-between py-2.5 sm:py-2 font-medium border-b">
-        <div>
-          <Link to="/">
-            <img src={logo} alt="logo" className="w-12 h-12" />
-          </Link>
-        </div>
+      <div className="flex items-center justify-between py-2.5 sm:py-2 font-medium">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-12 h-12" />
+        </Link>
 
-        <div>
-          <Link to="/login">Login</Link>
-        </div>
+        <nav>
+          {authUser ? (
+            <ul className="flex items-center gap-8">
+              <li>
+                <Link
+                  to="/license/add"
+                  className="text-gray-800 transition-colors duration-300 ease-in-out hover:text-purple-700 hover:underline"
+                >
+                  Add License
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/licenses/manage"
+                  className="text-gray-800 transition-colors duration-300 ease-in-out hover:text-purple-700 hover:underline"
+                >
+                  Manage License
+                </Link>
+              </li>
+              <li className="text-gray-800 transition-colors duration-300 ease-in-out hover:text-purple-700 cursor-pointer">
+                Logout
+              </li>
+            </ul>
+          ) : (
+            <Link
+              to="/login"
+              className="text-gray-800 transition-colors duration-300 ease-in-out hover:text-purple-700"
+            >
+              Login
+            </Link>
+          )}
+        </nav>
       </div>
     </div>
   );
